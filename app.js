4f283f6011050d5de97687ba2c4ad9d043c468c9
Natalie Voropaieva <voropaieva2603@gmail.com>
2022-08-03 23:29:24 +0300

    fix rect

--- app.js
+++ app.js
@@ -38,19 +38,17 @@ const getRandomNumber = (min,max) => {
     return Math.round(Math.random() * (max - min) + min)
 };
 
-const {width, height} = board.getBoundingClientRect()
-
 const createRandomCircle = () => {
+    const {width, height} = board.getBoundingClientRect();
     const circle = document.createElement('div')
-    const size = getRandomNumber(20, 60)
-    const x = getRandomNumber(0, width - size)
-    const y = getRandomNumber(0, height - size)
-
-    circle.classList.add('circle')
-    circle.style.width = `${size}px`
-    circle.style.height = `${size}px`
-    circle.style.top = `${y}px`
-    circle.style.left = `${x}px`
+    const size = getRandomNumber(20, 60);
+    const x = getRandomNumber(0, width - size);
+    const y = getRandomNumber(0, height - size);
+    circle.classList.add('circle');
+    circle.style.width = `${size}px`;
+    circle.style.height = `${size}px`;
+    circle.style.top = `${y}px`;
+    circle.style.left = `${x}px`;
 
     board.append(circle)
 };
